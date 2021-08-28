fa526ceb29422d141b8ccad2086f89df241f6bae
Richie-Z <zakariarichie@yahoo.com>
2021-08-28 14:26:17 +0700

    add code validator

--- src/main/java/com/richie/nhdownloder/url/CodeValidator.java
+++ src/main/java/com/richie/nhdownloder/url/CodeValidator.java
@@ -18,16 +18,29 @@ public class CodeValidator {
     }
 
     private final String validation(String c) {
-        System.out.println(c.toCharArray()[0]);
+        if (c.toCharArray()[0] == '#') {
+            return this.validInteger(c.substring(1));
+        }
+        return this.validInteger(c);
+    }
+
+    private final String validInteger(String c) {
+        try {
+            int newInt = Integer.parseInt(c);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not valid int : " + ex);
+            return "";
+        }
         return c;
     }
+
     @Override
     public String toString() {
         return code;
     }
 
     public static void main(String[] args) {
-        CodeValidator cv = new CodeValidator("#111");
+        CodeValidator cv = new CodeValidator("111");
         System.out.println(cv);
     }
 }
