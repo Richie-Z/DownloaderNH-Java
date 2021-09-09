3384b9e6ee5263dbf4c61e5f6a490882fc73a0af
Richie-Z <zakariarichie@yahoo.com>
2021-09-10 02:52:33 +0700

    Update scrapper

--- src/main/java/com/richie/nhdownloder/scraper/Scraper.java
+++ src/main/java/com/richie/nhdownloder/scraper/Scraper.java
@@ -8,6 +8,10 @@ package com.richie.nhdownloder.scraper;
 import com.richie.nhdownloder.url.CodeValidator;
 import com.richie.nhdownloder.url.FetchHTML;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -20,13 +24,19 @@ public class Scraper {
 
     private Document doc;
     private StringBuilder title = new StringBuilder();
-    public Scraper(Document d) {
+    private List<String> info = new ArrayList<String>();
+    private HashMap<String, Object> tags = new HashMap<>();
+    private String code = "";
+
+    public Scraper(Document d, String c) {
         this.doc = d;
+        this.code = c;
     }
 
     public String getTitles() {
         Element info = this.doc.getElementById("info");
         Elements titles = info.getElementsByClass("title");
+
         Elements docTitle = titles.get(0).children();
         for (Element title : docTitle) {
             this.title.append(title.html().concat(" "));
@@ -35,10 +45,36 @@ public class Scraper {
         return this.title.toString();
     }
 
+    public void getTag() {
+        Elements tags = this.doc.getElementById("info").getElementById("tags").getElementsByClass("tag-container");
+        tags.forEach((Element i) -> {
+            String[] splited = i.html().split(":");
+            this.tags.put(splited[0], this.tagInformation(splited[1]));
+        });
+    }
+
+    public String tagInformation(String s) {
+        Document dc = Jsoup.parseBodyFragment(s);
+        Elements name = dc.getElementsByClass("name");
+        return name.html();
+    }
+
+    public String getPictureURL() {
+        if (this.tags.isEmpty()) {
+            this.getTag();
+        }
+        int pages = Integer.parseInt(this.tags.get("Pages").toString());
+        for (int i = 1; i <= pages; i++) {
+            System.out.printf("https://i.nhentai.net/galleries/%s/%d.jpg %n", this.code, i);
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws IOException {
         FetchHTML fh = new FetchHTML(new CodeValidator("371274").toString());
-        Scraper sc = new Scraper(fh.main());
+        Scraper sc = new Scraper(fh.main(), "371274");
         sc.getTitles();
+        sc.getPictureURL();
     }
 
 }
